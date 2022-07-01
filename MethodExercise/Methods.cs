4cdb1c7defd98aa96a1ea588f7770551fe78f809
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-01 16:15:38 -0500

    Added second exercise

--- MethodExercise/Methods.cs
+++ MethodExercise/Methods.cs
@@ -15,5 +15,31 @@ public class Methods
         return input;
     }
 
+    public void Add()
+    {
+        
+        Console.WriteLine("\n\nWe are going to add some number today. Please enter two numbers to add ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        int sum = x + y;
+        Console.WriteLine($"The sum is: {sum}");
+    }
+    public void Subtract(int x, int y)
+    {
+        int result = x - y;
+        Console.WriteLine("Now onto subtraction, Im going to pick these two numbers because the above selections were terrible");
+        Console.WriteLine($"The value of {x} - {y} is: {result}");
+        
+    }
+    public int Mulitply(int x, int y)
+    {
+        return x * y;
+    }
+    public int Divide(int x, int y)
+    {
+        return x/y;
+    }
+
+
 
 }
--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -3,11 +3,26 @@
 Methods method = new Methods();
 
 var name = method.GetString("what is your name");
-var color = method.GetString("What is your favorite Color");
+var color = method.GetString("what is your favorite color");
 var animal = method.GetString("what is your favorite animal");
-var profession = method.GetString("What do you do for work");
+var profession = method.GetString("what do you do for work");
 
-string newString = ($"Today we are going to learn about {name}. {name}'s favorite color is {color} and his favoite animal is {animal}. he is an aspiring {profession}," +
+string newstring = ($"Today we are going to learn about {name}. {name}'s favorite color is {color} and his favoite animal is {animal}. He is an aspiring {profession}," +
     $"and hopes to one day make a living doing just that.");
 
-Console.WriteLine(newString);
+Console.WriteLine(newstring);
+
+
+//exercise two
+ 
+int x = 45;
+int y = 5;
+method.Add();
+method.Subtract(54,24);
+
+var product = method.Mulitply(x, y);
+var quotient = method.Divide(x, y);
+var methodProduct = method.Mulitply(product, quotient);
+
+Console.WriteLine($"\nNext we will do some Multiplication and Division of two numbers {x} and {y}. " +
+    $"the product of these two numbers is {product} and the quotient is: {quotient}. and if we wanted to multiply those two values we could do that also which is: {methodProduct}");
