f8d1c9dcce3327bb7d1e3512dd8bfa836f38364e
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-01 15:45:58 -0500

    Add project files.

---
+++ MethodExercise/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise;
+
+public class Methods
+{
+    public string GetString(string prompt)
+    {
+        Console.WriteLine(prompt);
+        var input = Console.ReadLine();
+        return input;
+    }
+
+
+}
---
+++ MethodExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using MethodExercise;
+
+Methods method = new Methods();
+
+var name = method.GetString("what is your name");
+var color = method.GetString("What is your favorite Color");
+var animal = method.GetString("what is your favorite animal");
+var profession = method.GetString("What do you do for work");
+
+string newString = ($"Today we are going to learn about {name}. {name}'s favorite color is {color} and his favoite animal is {animal}. he is an aspiring {profession}," +
+    $"and hopes to one day make a living doing just that.");
+
+Console.WriteLine(newString);
